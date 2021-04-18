f8ae446ca871faecafdfb1cbcc7eef7941f9ec94
GioEpic123 <giovanni.quevedo5@gmail.com>
2021-04-18 01:34:36 -0700

    merge conflict fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,13 @@ const connectMongo = require('./config/Mongo.js')
 const subscriptionRoutes = require('./routes/subscriptionRoutes')
 const reminderRoutes = require('./routes/reminderRoutes')
 const authRoutes = require('./routes/authRoutes')
+const cors = require('cors')
 
 dotenv.config()
 connectMongo()
 
 app.use(express.json())
+app.use(cors({ origin: true, credentials: true }))
 
 app.use('/api/v1/subscriptions', subscriptionRoutes)
 app.use('/api/v1/reminders', reminderRoutes)
@@ -19,3 +21,7 @@ app.use('/api/v1', authRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, (err) => console.log(`${err ? err : `Running on port ${PORT}`}`))
+
+process.on('uncaughtException', function (err) {
+	console.log(err)
+})
