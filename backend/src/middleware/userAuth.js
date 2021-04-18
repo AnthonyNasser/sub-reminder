77e9e9e96bf7b318d85ad5078ee42e34cc15979e
Andy <andy.lee.zhou@gmail.com>
2021-04-18 01:22:06 -0700

    implented redux on the frontend (not fully working), userAuth middleware on the backend

---
+++ backend/src/middleware/userAuth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+
+
+const userAuth = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (authHeader) {
+        jwt.verify(authHeader, process.env.JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+                return res.status(403).json({
+                    success: false,
+                    message: 'Nah bruh'
+                })
+            }
+            req.userId = decodedToken.id
+            next()
+        })
+    } else {
+        return res.status(401).json({
+            success: false,
+            message: "Not Authorized"
+        })
+    }
+}
+module.exports = {
+    userAuth
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,151 +3,185 @@ const router = express()
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const generateToken = require('../utils/generateToken')
-
+const userAuth = require('../middleware/userAuth').userAuth
 
 // Auth (Login) Route
-router.route('/login').post(asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+router.route('/login').post(
+	asyncHandler(async (req, res) => {
+		const { email, password } = req.body
 
-	if (!email) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid email',
-		})
-		return
-	}
-	if (!password) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid password',
-		})
-		return
-	}
-
-	const user = await User.findOne({ email }, (err, user) => {
-		if (err) {
-			res.status(500)
-			throw new Error('No email found')
-		}
-	})
-    
-    if (user && (await user.matchPassword(password))) { 
-        return res.status(200).json({
-			success: true,
-			message: 'Login successful',
-			token: generateToken(user._id),
-		})
-    } else {
-        return res.status(401).json({
-			success: false,
-			message: 'User Authorization failed',
-		})
-    }
-}))
-
-// Register Route
-router.route('/register').post(asyncHandler(async (req, res) => {
-	const { firstName, lastName, username, email, phoneNumber, password, confPassword } = req.body
-
-	// Guard Clauses
-	if (!firstName) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid firstName',
-		})
-		return
-	}
-	if (!lastName) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid lastName',
-		})
-		return
-	}
-	if (!username) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid username',
-		})
-		return
-	}
-	if (!email) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid email',
-		})
-		return
-	}
-	if (!phoneNumber) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid phoneNumber',
-		})
-		return
-	}
-	if (!password) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid password',
-		})
-		return
-	}
-	if (!confPassword) {
-		res.status(400).json({
-			success: false,
-			message: 'Invalid password',
-		})
-		return
-	}
-
-	// Check if User already exists by email
-	const userExists = await User.findOne({ email }, (err, user) => {
-		if (err) {
-			return res.status(500).json({
+		if (!email) {
+			res.status(400).json({
 				success: false,
-				message: 'No email found',
+				message: 'Invalid email',
+			})
+			return
+		}
+		if (!password) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid password',
+			})
+			return
+		}
+
+		const user = await User.findOne({ email }, (err, _user) => {
+			if (err) {
+				res.status(500)
+				throw new Error('No email found')
+			}
+		})
+
+		if (user && (await user.matchPassword(password))) {
+			return res.status(200).json({
+				success: true,
+				message: 'Login successful',
+				user: {
+					firstName: user.firstName,
+					lastName: user.lastName,
+					userId: user._id,
+					token: generateToken(user._id),
+				},
+			})
+		} else {
+			return res.status(401).json({
+				success: false,
+				message: 'User Authorization failed',
 			})
 		}
 	})
+)
+// router.route('/logout').delete(asyncHandler(async (req, res) => {
+// 	return res.status(200).json({
+// 		success: true,
+// 		message: 'Successfully Logged Out'
+// 	})
+// }))
 
-	if (userExists) {
-		return res.status(400).json({
-			success: false,
-			message: 'User with that email already exists',
-		})
-	}
+// Register Route
+router.route('/register').post(
+	asyncHandler(async (req, res) => {
+		const {
+			firstName,
+			lastName,
+			username,
+			email,
+			phoneNumber,
+			password,
+			confPassword,
+		} = req.body
+		// Guard Clauses
+		if (!firstName) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid firstName',
+			})
+			return
+		}
+		if (!lastName) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid lastName',
+			})
+			return
+		}
+		if (!username) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid username',
+			})
+			return
+		}
+		if (!email) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid email',
+			})
+			return
+		}
+		if (!phoneNumber) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid phoneNumber',
+			})
+			return
+		}
+		if (!password) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid password',
+			})
+			return
+		}
+		if (!confPassword) {
+			res.status(400).json({
+				success: false,
+				message: 'Invalid password',
+			})
+			return
+		}
 
-	if (password !== confPassword) {
-		return res.status(400).json({
-			success: false,
-			message: 'Passwords do not match',
+		// Check if User already exists by email
+		const userExists = await User.findOne({ email }, (err, user) => {
+			if (err) {
+				return res.status(500).json({
+					success: false,
+					message: 'No email found',
+				})
+			}
 		})
-	}
-	
-	// Continue with creation if they don't
-	const user = await User.create({
-		firstName,
-		lastName,
-		username,
-		email,
-		phoneNumber,
-		password,
+
+		if (userExists) {
+			return res.status(400).json({
+				success: false,
+				message: 'User with that email already exists',
+			})
+		}
+
+		if (password !== confPassword) {
+			return res.status(400).json({
+				success: false,
+				message: 'Passwords do not match',
+			})
+		}
+
+		// Continue with creation if they don't
+		const user = await User.create({
+			firstName,
+			lastName,
+			username,
+			email,
+			phoneNumber,
+			password,
+		})
+
+		if (user) {
+			user.password = undefined
+			return res.status(201).json({
+				success: true,
+				message: 'User successfully registered',
+				user: {
+					firstName: user.firstName,
+					lastName: user.lastName,
+					userId: user._id,
+					token: generateToken(user._id),
+				},
+			})
+		} else {
+			return res.status(500).json({
+				success: false,
+				message: 'Error registering User',
+			})
+		}
 	})
+)
 
-	if (user) {
-		user.password = undefined
-		return res.status(201).json({
-			success: true,
-			message: 'User successfully registered',
-			user,
-			token: generateToken(user._id),
-		})
-	} else {
-		return res.status(500).json({
-			success: false,
-			message: 'Error registering User',
-		})
-	}
-}))
+router.route('/testRoute').get(
+	userAuth,
+	asyncHandler(async (req, res) => {
+		console.log(req.userId, 'requ ser')
+		res.status(200).send('Ants a hoe')
+	})
+)
 
 module.exports = router
